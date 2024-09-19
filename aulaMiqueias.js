1c67d39c3cd47deacdab8096f176e20c1996a560
ricardojr.unifor@gmail.com <casllostreetwear12@gmail.com>
2024-09-18 22:08:58 -0300

    adicionado o estilo

--- aulaMiqueias.js
+++
@@ -1,6 +0,0 @@
-let N = parseInt(prompt("Por favor, insira um número:"));
-
-if (N > 0) {
-  N = N * (N - 1) * (N - 2) * (N - 3);
-}
-console.log(N);
--- aulaMiqueias.py
+++
@@ -1,4 +0,0 @@
-N = int(input("Insira um número: "))
-if N > 0:
-    N = N * (N - 1) * (N - 2) * (N - 3)
-print(N)
